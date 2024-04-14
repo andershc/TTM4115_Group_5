9adca254f6b3a4e5df1ab7b2d88e59333a951b5f
IvarHansgard <ivarhan@stud.ntnu.no>
2024-04-14 16:20:35 +0200

    code working on raspberryPi

--- raspberryPi/chargerFirmware.py
+++ raspberryPi/chargerFirmware.py
@@ -1,9 +1,5 @@
-Idle
-Start_lading
-Ferdig_lading
-Error_lading
-
 from sense_hat import SenseHat
+import time as t
 import random 
 sense = SenseHat()
 
@@ -12,21 +8,32 @@ green = (0,255,0)
 blue = (0,0,255)
 white = (255,255,255)
 clear = (0,0,0)
+sense.clear()
+chargerState = "idle"
+def getConnected():
+    return "connected"
 
-def checkForConnection():
+def getChargerState(charger):
+    return chargerState
+
+def changeState(state):
+    chargerState = state
+    print(chargerState)
     
+def checkForConnection():
+    print("check for connection")
 
 def select_charger(charger_id = 0):
     run = True
     x = 0
-    y = select_charger*2
-    charger = select_charger 
+    y = charger_id*2
+    charger = charger_id
     sense.set_pixel(x, y, white)
     sense.set_pixel(x, y+1, white)
 
     while run:
-        event = sense.stick.get_event()
-        if event.direction == "up":
+        event = sense.stick.wait_for_event()
+        if event.direction == "up" and event.action == "pressed":
             sense.set_pixel(x, y, clear)
             sense.set_pixel(x, y+1, clear)
             y = y-2
@@ -36,7 +43,7 @@ def select_charger(charger_id = 0):
                 charger = 3
             sense.set_pixel(x, y, white)
             sense.set_pixel(x, y+1, white)
-        elif event.direction == "down":
+        elif event.direction == "down" and event.action == "pressed":
             sense.set_pixel(x, y, clear)
             sense.set_pixel(x, y+1, clear)
             y = y+2
@@ -46,26 +53,26 @@ def select_charger(charger_id = 0):
                 charger = 0
             sense.set_pixel(x, y, white)
             sense.set_pixel(x, y+1, white)
-        elif event.action == "pressed":
-            if getChargerState(charger) == "connected":
+        elif event.direction == "middle" and event.action == "pressed":
+            if getConnected() == "connected":
                 run = False
             else:
                 print("Error: charger not connected")
                 sense.set_pixel(x, y, red)
                 sense.set_pixel(x, y+1, red)
-                sleep(0.5)
+                t.sleep(0.5)
                 sense.set_pixel(x, y, white)
                 sense.set_pixel(x, y+1, white)
-                sleep(0.5)
+                t.sleep(0.5)
                 sense.set_pixel(x, y, red)
                 sense.set_pixel(x, y+1, red)
-                sleep(0.5)
+                t.sleep(0.5)
                 sense.set_pixel(x, y, white)
                 sense.set_pixel(x, y+1, white)
-        sleep(0.1)
+        t.sleep(0.5)
 
-    changeState(start_charging(charger))
-
+    changeState("charging")
+    start_charging(charger)
 
 def start_charging(charger_id):
     run = True
@@ -78,25 +85,32 @@ def start_charging(charger_id):
         sense.set_pixel(i, y+1, green)
         x = i
     x = x+1
-    chargeTimeOut = 0
+    chargeTime = 0
     is_error = random.randint(0,11)
     if is_error == 5:
         changeState(error_charger(charger))
     print("Started charging on charger ", charger_id)
     while run:
-        while chargeTimeOut!= 100:
+        while chargeTime!= 5:
             sense.set_pixel(x, y, clear)
             sense.set_pixel(x, y+1, clear)
-            sleep(0.5)
+            t.sleep(0.5)
+            sense.set_pixel(x, y, green)
+            sense.set_pixel(x, y+1, green)
+            t.sleep(1)
+            sense.set_pixel(x, y, clear)
+            sense.set_pixel(x, y+1, clear)
+            t.sleep(0.5)
             sense.set_pixel(x, y, green)
             sense.set_pixel(x, y+1, green)
             chargeTime = chargeTime+1
         x = x+1
-        chargeTimeOut = 0
-        if x == 7:
+        chargeTime = 0
+        if x == 8:
             run = False
-    changeState(finished_charging(charger))
-        
+    finished_charging(charger)
+
+
 def error_charger(charger_id):
     print("Charging error on ", charger_id)
     run = True
@@ -105,7 +119,7 @@ def error_charger(charger_id):
     for i in range(1,8):
         sense.set_pixel(i, y, red)
         sense.set_pixel(i, y+1, red)
-    setChargerState(charger_id, "error")
+    changeState("error")
     while run:
         print("error loop")
         
@@ -117,10 +131,15 @@ def finished_charging(charger_id):
     run = True
     x = 0
     y = charger_id*2
-    setChargerState(charger_id, "finished")
+    changeState("finished")
     while run:
-        event = sense.stick.get_event()
-        if event.action == "pressed" or getCallFromServer() == "stop":
-            for i in range(1,8):
+        event = sense.stick.wait_for_event()
+        if event.direction == "middle" and event.action == "pressed":
+            for i in range(0,8):
                 sense.set_pixel(i, y, clear)
                 sense.set_pixel(i, y+1, clear)
+
+def main():
+    select_charger()
+
+main()
