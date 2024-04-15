658174dbae5edf2d4f9d3a7d45fc87e4dd298b4b
IvarHansgard <ivarhan@stud.ntnu.no>
2024-04-15 13:56:47 +0200

    :D

--- raspberryPi/chargerFirmware.py
+++ raspberryPi/chargerFirmware.py
@@ -110,7 +110,7 @@ class charger:
         while run:
             event = sense.stick.wait_for_event()
             if event.direction == "middle" and event.action == "pressed":
-                for i in range(0,8):
+                for i in range(1,8):
                     sense.set_pixel(i, y, clear)
                     sense.set_pixel(i, y+1, clear)
         self.changeState("idle")
@@ -128,42 +128,43 @@ def changeChargerState(charger, state):
     charger.changeState(state)
 
 def selectCharger(chargers):
-    run = True
-    x = 0
-    y = 0
-    charger = 0
-    chargerArray = chargers 
-    sense.set_pixel(x, y, white)
-    sense.set_pixel(x, y+1, white)
+    while True:
+        run = True
+        x = 0
+        y = 0
+        charger = 0
+        chargerArray = chargers 
+        sense.set_pixel(x, y, white)
+        sense.set_pixel(x, y+1, white)
 
-    while run:
-        event = sense.stick.wait_for_event()
-        if event.direction == "up" and event.action == "pressed":
-            sense.set_pixel(x, y, clear)
-            sense.set_pixel(x, y+1, clear)
-            y = y-2
-            charger = charger-1
-            if y < 0:
-                y = 6
-                charger = 3
-            sense.set_pixel(x, y, white)
-            sense.set_pixel(x, y+1, white)
-        elif event.direction == "down" and event.action == "pressed":
-            sense.set_pixel(x, y, clear)
-            sense.set_pixel(x, y+1, clear)
-            y = y+2
-            charger = charger+1
-            if y > 6:
-                y = 0
-                charger = 0
-            sense.set_pixel(x, y, white)
-            sense.set_pixel(x, y+1, white)
-        elif event.direction == "middle" and event.action == "pressed":
-            if chargerArray[charger].getConnected():
-                run = False
-        t.sleep(0.5)
+        while run:
+            event = sense.stick.wait_for_event()
+            if event.direction == "up" and event.action == "pressed":
+                sense.set_pixel(x, y, clear)
+                sense.set_pixel(x, y+1, clear)
+                y = y-2
+                charger = charger-1
+                if y < 0:
+                    y = 6
+                    charger = 3
+                sense.set_pixel(x, y, white)
+                sense.set_pixel(x, y+1, white)
+            elif event.direction == "down" and event.action == "pressed":
+                sense.set_pixel(x, y, clear)
+                sense.set_pixel(x, y+1, clear)
+                y = y+2
+                charger = charger+1
+                if y > 6:
+                    y = 0
+                    charger = 0
+                sense.set_pixel(x, y, white)
+                sense.set_pixel(x, y+1, white)
+            elif event.direction == "middle" and event.action == "pressed":
+                if chargerArray[charger].getConnected():
+                    run = False
+            t.sleep(0.5)
 
-    changeChargerState(chargerArray[charger], "charging")
+        changeChargerState(chargerArray[charger], "charging")
 
 
     
